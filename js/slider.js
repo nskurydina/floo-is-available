7f2e65eeace03f42527722051d6dea8c506e30f0
nskurydina <95338423+nskurydina@users.noreply.github.com>
2023-11-16 11:55:04 +0300

    Create slider.js

---
+++ js/slider.js
@@ -0,0 +1,71 @@
+const Slider = (function () {
+    function Slider__showImage(evt) {
+        if (evt.target != this) {
+            this.previousElementSibling.firstElementChild.src = evt.target.href;
+            let el;
+            for (let i = 0; i < this.childElementCount; ++i) {
+                el = this.children[i];
+                el.className = (el == evt.target) ? 'active' : '';
+            }
+            evt.preventDefault();
+        }
+    }
+    function Slider(buttonset, config) {
+        this.buttonset = buttonset;
+        this.buttonset.addEventListener('click', Slider__showImage);
+        if (!config.initial)
+            config.initial = 0;
+        this.buttonset.classList.add('slider');
+        let button;
+        config.images.forEach((el, i) => {
+            button = document.createElement('a');
+            button.href = el;
+            button.textContent = i + 1;
+            if (i == config.initial)
+                button.classList.add('active');
+            this.buttonset.appendChild(button);
+        });
+        const panel = document.createElement('section');
+        panel.classList.add('slider-panel');
+        const img = document.createElement('img');
+        img.src = config.images[config.initial];
+        panel.appendChild(img);
+        this.buttonset.insertAdjacentElement('beforebegin', panel);
+    }
+    function Slider__getCurrentIndex() {
+        for (let i = 0; i < this.buttonset.childElementCount; ++i) {
+            if (this.buttonset.children[i].classList.contains('active'))
+                return i;
+        }
+    }
+    function Slider__setCurrentIndex(ind) {
+        if (ind < 0)
+            ind = 0;
+        if (ind > this.buttonset.childElementCount - 1)
+            ind = this.buttonset.childElementCount - 1;
+        this.buttonset.previousElementSibling.firstElementChild.src = this.buttonset.children[ind].href;
+        for (let i = 0; i < this.buttonset.childElementCount; ++i)
+            this.buttonset.children[i].className = (i == ind) ? 'active' : '';
+    }
+    Object.defineProperty(Slider.prototype, 'currentIndex', {
+        get: Slider__getCurrentIndex,
+        set: Slider__setCurrentIndex
+    });
+    return Slider;
+})();
+const Slider2 = (function () {
+    function Slider2(buttonset) {
+        if (typeof buttonset == 'string')
+            buttonset = document.getElementById(buttonset);
+        Slider.apply(this, arguments);
+    }
+    Slider2.prototype = Object.create(Slider.prototype);
+    Slider2.prototype.constructor = Slider2;
+    Slider2.prototype.goNext = function () {
+        this.currentIndex = this.currentIndex + 1;
+    };
+    Slider2.prototype.goPrevious = function () {
+        this.currentIndex = this.currentIndex - 1;
+    };
+    return Slider2;
+})();
